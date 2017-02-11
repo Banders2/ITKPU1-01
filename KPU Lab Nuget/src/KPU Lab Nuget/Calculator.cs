75a8d37d62a353bbbf00bc5bb4dddf6491fb56c2
Anders Meidahl <Andersmeidahl@gmail.com>
2017-02-11 14:11:21 +0100

    Add simple calculator

---
+++ KPU Lab Nuget/src/KPU Lab Nuget/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KPU_Lab_Nuget
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Substract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
